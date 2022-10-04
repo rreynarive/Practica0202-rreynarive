3057b5b14377ebf9931f097c6c758cb5d301cf69
Rosa <rreynarive1@educacion.navarra.es>
2022-10-04 17:12:52 +0200

    verificar 11

--- Practica0202_rreynarive/Ejercicio11.py
+++ Practica0202_rreynarive/Ejercicio11.py
@@ -1,2 +1,12 @@
 #Cuenta de ahorro
 intereses_anuales = 4/100
+deposito = float(input("Deposite su dinero:\n"))
+calculo = deposito + (deposito*intereses_anuales)
+final = round(calculo, 2)
+calculo2 = deposito + (deposito*intereses_anuales*2)
+final2 = round(calculo2, 2)
+calculo3 = deposito + (deposito*intereses_anuales*3)
+final3 = round(calculo3, 2)
+print("Su ahorro en el primer año es", final)
+print("Su ahorro en el segundo año es", final2)
+print("Su ahorro en el tercer año es", final3)
