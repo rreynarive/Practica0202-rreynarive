b6a2b7aadf566d025c48d6bc83a936b481b3d9eb
Rosa <rreynarive1@educacion.navarra.es>
2022-10-04 16:11:22 +0200

    ejercicio 9 acabado

--- Practica0202_rreynarive/Ejercicio9.py
+++ Practica0202_rreynarive/Ejercicio9.py
@@ -1,3 +1,6 @@
 euros = float(input("Cantidad a invertir:"))
-interes = float(input("Interes anual:"))
+interes = float(input("Interes anual(%):"))
 años = int(input("Cuantos años:"))
+
+capital = euros + ((euros*interes*años)/100)
+print("La capital de la inversion es de", capital, "€")
