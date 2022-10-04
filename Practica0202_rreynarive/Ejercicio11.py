1768ac3cf5a9d165887ef57d2a349dd6514ed891
Rosa <rreynarive1@educacion.navarra.es>
2022-10-04 16:56:07 +0200

    ejercicio 12 acabado

---
+++ Practica0202_rreynarive/Ejercicio11.py
@@ -0,0 +1,2 @@
+#Cuenta de ahorro
+intereses_anuales = 4/100
---
+++ Practica0202_rreynarive/Ejercicio12.py
@@ -0,0 +1,13 @@
+inicio = 3.49
+pan_nofresco = (inicio-(inicio*0.6))
+final = round(pan_nofresco, 2)
+
+compra = int(input("¿Cuantas barras va a comprar?"))
+
+print("Precio habitual de una barra de pan es", inicio, "€")
+print("Por no ser fresco el precio es", final, "€")
+
+operacion = compra * final
+pa_final = round(operacion, 2)
+
+print("El coste de su compra es", pa_final, "€")
